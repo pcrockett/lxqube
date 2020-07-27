712398c7a28979adcc5680b1934d00f4040dafc0
Philip Crockett <contact@philcrockett.com>
2020-07-27 17:09:14 +0200

    lxq template edit

--- src/lxq-template-edit.sh
+++ src/lxq-template-edit.sh
@@ -20,8 +20,6 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -Eeuo pipefail
 
-[[ "${BASH_VERSINFO[0]}" -lt 4 ]] && echo "Bash >= 4 required" && exit 1
-
 readonly SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 readonly SCRIPT_NAME=$(basename "$0")
 readonly DEPENDENCIES=()
@@ -40,14 +38,7 @@ for dep in "${DEPENDENCIES[@]}"; do
 done
 
 function show_usage() {
-    printf "Usage: lxq template [command]\n" >&2
-    printf "\n" >&2
-    printf "Available commands:\n" >&2
-    printf "  list\t\tList templates\n" >&2
-    printf "  create\tCreate a template\n" >&2
-    printf "  destroy\tDestroy a template\n" >&2
-    printf "  attach\tAttach a terminal to a template\n" >&2
-    printf "  edit\t\tEdit LXC config for a template\n" >&2
+    printf "Usage: lxq template edit [template-name]\n" >&2
     printf "\n" >&2
     printf "Flags:\n">&2
     printf "  -h, --help\t\tShow help message then exit\n" >&2
@@ -70,40 +61,20 @@ function is_set() {
 
 function parse_commandline() {
 
-    if [ "${#}" -gt "0" ]; then
-        case "$1" in
-            list)
-                LXQ_COMMAND="list"
-            ;;
-            create)
-                LXQ_COMMAND="create"
-            ;;
-            destroy)
-                LXQ_COMMAND="destroy"
-            ;;
-            attach)
-                LXQ_COMMAND="attach"
-            ;;
-            edit)
-                LXQ_COMMAND="edit"
-            ;;
-        esac
-
-        if is_set "${LXQ_COMMAND+x}"; then
-            return
-        fi
-    fi
-
     while [ "${#}" -gt "0" ]; do
         local consume=1
 
-        case "$1" in
+        case "${1}" in
             -h|-\?|--help)
                 ARG_HELP="true"
             ;;
             *)
-                echo "Unrecognized argument: ${1}"
-                show_usage_and_exit
+                if is_set "${ARG_TEMPLATE_NAME+x}"; then
+                    echo "Unrecognized argument: ${1}"
+                    show_usage_and_exit
+                else
+                    ARG_TEMPLATE_NAME="${1}"
+                fi
             ;;
         esac
 
@@ -113,16 +84,24 @@ function parse_commandline() {
 
 parse_commandline "$@"
 
-if is_set "${LXQ_COMMAND+x}"; then
-
-    shift 1
-    "${SCRIPT_DIR}/lxq-template-${LXQ_COMMAND}.sh" "$@"
-    exit "${?}"
-fi
-
 if is_set "${ARG_HELP+x}"; then
     show_usage_and_exit
 fi;
 
-echo "No arguments specified."
-show_usage_and_exit
+if is_set "${ARG_TEMPLATE_NAME+x}"; then
+    CONFIG_PATH="${LXQ_PATH}/lxq-templ-${ARG_TEMPLATE_NAME}/config"
+    if [ ! -f "${CONFIG_PATH}" ]; then
+        panic "${CONFIG_PATH} does not exist."
+    fi
+
+    if is_set "${EDITOR+x}"; then
+        "${EDITOR}" "${CONFIG_PATH}"
+    elif installed "nano"; then
+        nano "${CONFIG_PATH}"
+    else
+        panic "No text editor defined. Set EDITOR environment variable to your desired text editor."
+    fi
+else
+    echo "No template name specified."
+    show_usage_and_exit
+fi
